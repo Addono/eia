dd9b7e33fa744675a15c6724e495a063dd736170
Addono <a.d.knapen@protonmail.com>
2017-09-24 19:46:47 +0200

    'dev'

---
+++ graph.js
@@ -0,0 +1,120 @@
+/*global Viva*/
+var graph = Viva.Graph.graph();
+
+// Assumed to be a tree
+var nodeData = {
+    'root': {
+        'target': null,
+        'label' : 'test',
+    },
+    'test': {
+        'target': 'root',
+        'label' : 'test',
+    },
+    'test2': {
+        'target': 'root',
+        'label' : 'test',
+    },
+    'test3': {
+        'target': 'root',
+        'label' : 'test',
+    },
+    'test4': {
+        'target': 'root',
+        'label' : 'test',
+    },
+    'test5': {
+        'target': 'test3',
+        'label' : 'test',
+    },
+    'test6': {
+        'target': 'test5',
+        'label' : 'test',
+    },
+    'test7': {
+        'target': 'test5',
+        'label' : 'test',
+    },
+    'test8': {
+        'target': 'test5',
+        'label' : 'test',
+    },
+};
+
+var initNodes = [
+    'root',
+    'test',
+    'test2',
+    'test3',
+];
+
+for (i = 0; i < initNodes.length; i++) {
+    var name = initNodes[i];
+    spawnNode(name);
+}
+
+function spawnNode(name) {
+    var data = {
+        width: 25,
+        height: 25,
+        type: 'test',
+        target: nodeData[name].target,
+        label: nodeData[name].label
+    };
+
+    graph.addNode(name, data);
+
+    var target = nodeData[name].target;
+    if (target !== null) {
+        graph.addLink(name, target);
+    }
+}
+
+function onLoad() {
+    var layout = Viva.Graph.Layout.forceDirected(graph, {
+        springLength : 80,
+        springCoeff : 0.0008,
+        dragCoeff : 0.02,
+        gravity : -1.2
+    });
+
+    var graphics = Viva.Graph.View.svgGraphics();
+    graphics.node(createNodeUI).placeNode(placeNode);
+
+    var renderer = Viva.Graph.View.renderer(graph, {
+        layout     : layout,
+        container  : document.getElementById('graphContainer'),
+        graphics   : graphics
+    });
+    renderer.run();
+
+    function createNodeUI(node){
+        var ui = Viva.Graph.svg('g');
+        var svgText = Viva.Graph.svg('text')
+            .attr('y', '-4px')
+            .attr('fill', '#cccccc')
+            .text(node.data.label)
+        ;
+        var img = Viva.Graph.svg('image')
+                .attr('width', 32)
+                .attr('height', 32)
+                .link('img/' + node.data.type + '.jpg')
+        ;
+
+        ui.append(svgText);
+        ui.append(img);
+
+        ui.data = node.data;
+
+        var pos = layout.getNodePosition(node.id);
+        // placeNode(ui, pos);
+        return ui;
+    }
+
+    function placeNode(nodeUI, pos) {
+        nodeUI.attr('transform',
+            'translate(' +
+            (pos.x - 16) + ',' + (pos.y - 16) +
+            ')');
+    }
+}
